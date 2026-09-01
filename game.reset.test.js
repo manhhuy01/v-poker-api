9f0ac8b970e527d10db1a5e98fffba9d5ec92ff4
HuyNguyen <huy.nguyen@vexere.com>
2026-09-01 18:08:00 +0700

    fix reset game

--- game.reset.test.js
+++ game.reset.test.js
@@ -91,8 +91,8 @@ describe('game reset poker log', () => {
 
     expect(db.logPoker.mock.calls[0][0].data.table.actions).toEqual(
       [
-          { user: 'a', action: 'bet', amount: 4 },
-          { user: 'b', action: 'call', amount: 4 },
+        { user: 'a', action: 'bet', amount: 4 },
+        { user: 'b', action: 'call', amount: 4 },
       ]
     );
 
@@ -165,5 +165,75 @@ describe('game reset poker log', () => {
     expect(db.logPoker).not.toHaveBeenCalled();
     expect(db.logGame).not.toHaveBeenCalled();
     expect(db.updateBalance).toHaveBeenCalled();
+  });
+
+  test('restore balance when reset before game finishes', async () => {
+    const data = {
+      setting: { smallBlind: 1 },
+      players: [
+        { userName: 'a', accBalance: 20 },
+        { userName: 'b', accBalance: 20 },
+      ],
+      position: {
+        1: {
+          user: { userName: 'a', accBalance: 18, startBalance: 20 },
+          betBalance: 2,
+          isFold: false,
+          namePos: 'D',
+          cards: ['As', 'Ks'],
+          isThinking: false,
+          isPlaying: true,
+          winBalance: 0,
+          showCard: false,
+          action: 'bet',
+        },
+        2: {
+          user: { userName: 'b', accBalance: 19, startBalance: 20 },
+          betBalance: 1,
+          isFold: false,
+          namePos: '',
+          cards: ['Qs', 'Js'],
+          isThinking: true,
+          isPlaying: true,
+          winBalance: 0,
+          showCard: false,
+          action: 'call',
+        },
+        3: {},
+        4: {},
+        5: {},
+        6: {},
+        7: {},
+        8: {},
+        9: {},
+      },
+      table: {
+        start: true,
+        preFlop: true,
+        flop: ['2s', '3s', '4s'],
+        finish: false,
+        firstActionPlayer: 2,
+        pot: [{
+          users: [],
+          balance: 0,
+          isHavePlayerAllIn: false,
+        }],
+        currentBet: 2,
+        isShowDown: false,
+        showDownAt: undefined,
+      },
+      cards: ['7s', '8s'],
+    }
+
+    game.setData(data);
+    game.playerAction({ userName: 'b', type: 'CALL' });
+    game.playerAction({ userName: 'a', type: 'CHECK' });
+    await game.reset();
+
+    const afterResetData = game.getData();
+    expect(afterResetData.position[1].user.accBalance).toEqual(20);
+    expect(afterResetData.position[2].user.accBalance).toEqual(20);
+
+
   });
 })
